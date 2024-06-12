0e545d2399da4926c50183eb63076e9e9a930b46
nadaonfire <nhaeflig@zhdk.ch>
2024-06-12 21:14:12 +0200

    changes

---
+++ main.js
@@ -0,0 +1,111 @@
+const SPOTIFY_CLIENT_ID = "67b411e20d594f30bf7a8d3bbde54285";
+const SPOTIFY_CLIENT_SECRET = "161fc5e3df004b95af3ba8c62f3eaf54";
+const PLAYLIST_ID = "37i9dQZF1DWYoYGBbGKurt";
+const container = document.querySelector('div[data-js="tracks"]');
+
+function fetchPlaylist(token, playlistId) {
+  console.log("token: ", token);
+
+  fetch(`https://api.spotify.com/v1/playlists/${PLAYLIST_ID}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      if (data.tracks && data.tracks.items) {
+        data.tracks.items.forEach((item) => {
+          console.log(item.track.name);
+        });
+
+        addTracksToPage(data.tracks.items);
+        addTracksToSmallPage(data.tracks.items);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function addTracksToPage(tracks) {
+  const ul = document.createElement("ul");
+
+  tracks.forEach((track) => {
+    console.log("track: ", track);
+    const li = document.createElement("li");
+    li.classList.add("list-item");
+
+    // Create a span that holds the album name
+    li.innerHTML = `
+      <div class="flex-container-tracks">
+        ${track.track.album.images[0]
+          ? `<img class="albumCoverImage" src="${track.track.album.images[0].url}"></img>`
+          : "<p>No Image available</p>"
+        }
+        <span class="album">
+          <h2> Album: ${track.track.album.name} </h2>
+        </span>
+        <p class="artistName">${track.track.name} by ${track.track.artists
+          .map((artist) => artist.name)
+          .join(", ")}</p>
+      </div>
+    `;
+    
+    ul.appendChild(li);
+  });
+  container.appendChild(ul);
+}
+
+function addTracksToSmallPage(tracks) {
+  const ul = document.createElement("ul");
+
+  tracks.forEach((track) => {
+    console.log("track: ", track);
+    const li = document.createElement("li");
+    li.classList.add("list-item");
+
+    // Create a span that holds the album name
+    li.innerHTML = `
+      <div class="flex-container-tracks">
+        ${track.track.album.images[0]
+          ? `<img class="albumCoverImage" src="${track.track.album.images[0].url}"></img>`
+          : "<p>No Image available</p>"
+        }
+        <span class="album">
+          <h1> Album: ${track.track.album.name} </h1>
+        </span>
+        <p class="artistName">${track.track.name} by ${track.track.artists
+          .map((artist) => artist.name)
+          .join(", ")}</p>
+      </div>
+    `;
+    
+    ul.appendChild(li);
+  });
+  smallContainer.appendChild(ul);
+}
+
+function fetchAccessToken() {
+  fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=client_credentials&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.access_token) {
+        fetchPlaylist(data.access_token, PLAYLIST_ID);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+fetchAccessToken();
